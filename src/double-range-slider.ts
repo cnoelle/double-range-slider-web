6174b4d8c2c0df59efe4148f70fbb4ce99de1ec2
cnoelle <19640096+cnoelle@users.noreply.github.com>
2025-12-06 23:48:15 +0100

    remove utils

--- src/double-range-slider.ts
+++ src/double-range-slider.ts
@@ -1,5 +1,3 @@
-import { createElement } from "./utils.js";
-
 export class DoubleRangeSlider extends HTMLElement {
 
     static readonly #DEFAULT_TAG: string = "double-range-slider";
@@ -425,3 +423,41 @@ export class DoubleRangeSlider extends HTMLElement {
     }
 
 }
+
+function createElement<K extends keyof HTMLElementTagNameMap>(tagName: K, options?: ElementCreationOptions&{
+    text?: string;
+    title?: string;
+    id?: string;
+    classes?: string|Array<string>;
+    role?: string;
+    attributes?: Record<string, string>;
+    css?: Record<string, string>;
+    parent?: Node;
+}): HTMLElementTagNameMap[K] {
+    const el: HTMLElementTagNameMap[K] = document.createElement(tagName, options);
+    if (options?.text) {
+        if (tagName === "input")
+            (el as HTMLInputElement).value = options.text;
+        else
+            el.textContent = options?.text;
+    }
+    if (options?.title)
+        el.title = options.title;
+    if (options?.classes) {
+        if (typeof options.classes === "string")
+            el.classList.add(options.classes)
+        else
+            options.classes.forEach(cl => el.classList.add(cl));
+    }
+    if (options?.id)  // TODO some base id + sensible suffix
+        el.id = options.id;
+    if (options?.role)
+        el.role = options?.role;
+    if (options?.attributes)
+        Object.entries(options.attributes).forEach(([key, value]) => el.setAttribute(key, value));
+    if (options?.css)
+       Object.entries(options.css).forEach(([key, value]) => el.style.setProperty(key, value));
+    options?.parent?.appendChild(el);
+    return el;
+}
+
